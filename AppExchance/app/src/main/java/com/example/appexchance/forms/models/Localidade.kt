3f60407113f29fd1854aacf0d90d18c3e3c335c0
Beatriz Frederico <beatrizfrederico5@gmail.com>
2023-11-14 21:46:40 -0300

    Removendo arquivo do app

--- AppExchance/app/src/main/java/com/example/appexchance/forms/models/Localidade.kt
+++
@@ -1,9 +0,0 @@
-package com.example.appexchance.forms.models
-
-data class Localidade(
-    var idLocalidade: Int,
-    var pais: String,
-    var cidade: String,
-    var endereco: String,
-    var cep: String
-)
--- AppExchance/app/src/main/java/com/example/appexchance/forms/models/LoginRequest.kt
+++
@@ -1,3 +0,0 @@
-package com.example.appexchance.forms.models
-
-data class LoginRequest(val email: String, val senha: String)
